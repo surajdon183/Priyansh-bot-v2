f9c0e19e5c8e9ee6b5c8fb513d80964723e3e052
surajdon183 <mufyduzo@genzmaile.com>
2025-04-01 00:08:05 +0530

    Update autosetname.js

--- Priyansh/events/autosetname.js
+++ Priyansh/events/autosetname.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "autosetname",
 	eventType: ["log:subscribe"],
 	version: "1.0.3",
-	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+	credits: "suraj",
 	description: "Automatically set new member nicknames"
 };
 
