95a5afc2215f6e861ecd4ee0f0010a16a0881265
surajdon183 <mufyduzo@genzmaile.com>
2025-04-01 00:30:41 +0530

    Update 6packs.js

--- Priyansh/commands/6packs.js
+++ Priyansh/commands/6packs.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "6mui",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "suraj",
   description: "Random 6-pack photos",
   commandCategory: "Random-IMG",
   usages: "6mui",
