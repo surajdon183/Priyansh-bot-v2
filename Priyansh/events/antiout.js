8455608abe7771a023f139d81a6dc22afc81ea2e
surajdon183 <mufyduzo@genzmaile.com>
2025-04-01 00:06:06 +0530

    Update antiout.js

--- Priyansh/events/antiout.js
+++ Priyansh/events/antiout.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "antiout",
  eventType: ["log:unsubscribe"],
  version: "0.0.1",
- credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+ credits: "SURAJ",
  description: "Listen events"
 };
 
