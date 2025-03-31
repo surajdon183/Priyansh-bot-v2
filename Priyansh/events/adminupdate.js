af73cf69a6b7821789f0dec821620af28a29b5f5
surajdon183 <mufyduzo@genzmaile.com>
2025-04-01 00:03:18 +0530

    Update adminupdate.js

--- Priyansh/events/adminupdate.js
+++ Priyansh/events/adminupdate.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "adminUpdate",
 	eventType: ["log:thread-admins","log:thread-name", "log:user-nickname","log:thread-icon","log:thread-color"],
 	version: "1.0.1",
-	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+	credits: "SURAJ",
 	description: "Update team information quickly",
     envConfig: {
         sendNoti: true,
