2fa1d19117694290d9f973e42239f65df0106fe4
surajdon183 <mufyduzo@genzmaile.com>
2025-04-01 00:46:57 +0530

    Update adden.js

--- Priyansh/commands/adden.js
+++ Priyansh/commands/adden.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "anhdaden",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "suraj",
   description: "White brother :v",
   commandCategory: "Edit-IMG",
   usages: "[text 1] | [text 2]",
