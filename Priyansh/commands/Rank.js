75697aa4f12c6ae854acce2a916df4e29dd30ba5
surajdon183 <mufyduzo@genzmaile.com>
2025-04-01 00:38:32 +0530

    Update Rank.js

--- Priyansh/commands/Rank.js
+++ Priyansh/commands/Rank.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "rank",
 	version: "2.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "suraj",
 	description: "View Member Rankings",
 	commandCategory: "Group",
 	usages: " [user] or [tag]",
