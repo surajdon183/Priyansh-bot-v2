83747d35c410d7a51d03c0906b8399a2d3f817c6
surajdon183 <mufyduzo@genzmaile.com>
2025-04-01 00:42:11 +0530

    Update Work-beta.js

--- Priyansh/commands/Work-beta.js
+++ Priyansh/commands/Work-beta.js
@@ -3,7 +3,7 @@ module.exports.config = {
     name: "job",
     version: "1.0.2",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
+    credits: "suraj", 
     description: "",
     commandCategory: "Economy",
     cooldowns: 5,
