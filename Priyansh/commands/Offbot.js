80874f06b27bc1bb14412bc8923c3dc36ce7b3f9
surajdon183 <mufyduzo@genzmaile.com>
2025-04-01 00:37:15 +0530

    Update Offbot.js

--- Priyansh/commands/Offbot.js
+++ Priyansh/commands/Offbot.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "offbot",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "suraj",
 	description: "turn the bot off",
 	commandCategory: "system",
 	cooldowns: 0
