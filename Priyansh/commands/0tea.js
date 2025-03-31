b0bf4d72f29cd1e9bc6c2d88fc6945c0a893c9d0
surajdon183 <mufyduzo@genzmaile.com>
2025-04-01 00:28:58 +0530

    Update 0tea.js

--- Priyansh/commands/0tea.js
+++ Priyansh/commands/0tea.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "tea",
     version: "1.0.1",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
+  credits: "SURAJ", 
   description: "hihihihi",
   commandCategory: "no prefix",
   usages: "tea",
