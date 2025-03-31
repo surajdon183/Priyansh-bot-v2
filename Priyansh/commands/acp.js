7eca9973765cabe6fea94c1d8a3a905b9a3529ff
surajdon183 <mufyduzo@genzmaile.com>
2025-04-01 00:43:02 +0530

    Update acp.js

--- Priyansh/commands/acp.js
+++ Priyansh/commands/acp.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "acp",
   version: "1.0.0",
   hasPermssion: 2,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "suraj",
   description: "Make friends via Facebook id",
   commandCategory: "bot id",
   usages: "uid",
